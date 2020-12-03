fa4c9f63cac6559e34d91c12179668940d952f4e
YcVoz <32327418+YcVoz@users.noreply.github.com>
2020-12-04 02:42:52 +0800

    Add files via upload

--- Problem_2.py
+++ Problem_2.py
@@ -15,7 +15,8 @@ import random
 
 
 # In[2]:
-
+def phi(x):
+    return x.reshape(len(x), 1)
 
 def softmax(x):
     x_2 = x - np.max(x)
@@ -31,24 +32,57 @@ def get_y(data,n,k,w):
     
     return s
 
-def get_pred(data,w,n_class):
-    probability_max = 0
-    max_label = -1
-    for i in range(n_class):
-        probability = 0
-        s = 0
-        a_i = w[i].T.dot(data.reshape(-1,1))   
-        for j in range(n_class):
-            a_j = w[j].T.dot(data.reshape(-1,1))   
-            s += np.exp(a_i - a_j)
-        probability = 1/s
-        if(probability > 0.5):
-            return i
-        if(probability > probability_max):
-            probability_max = probability
-            max_label = i
+def hessian(w, k, X):
 
-    return max_label
+    output = np.zeros((len(w[0]), len(w[0])))
+    for n in range(len(X)):
+        scale = compute_y(n, k, w, X) * (1 - compute_y(n, k, w, X))
+        output += scale * (phi(X[n]).dot(phi(X[n]).T))
+    return output
+
+def compute_y(n, k, w, X):  # 公式裡的y
+    s = np.float64(0.)
+    ak = w[k].T.dot(phi(X[n]))
+    # target classes
+    for j in range(5):
+        aj = w[j].T.dot(phi(X[n]))
+        s += np.nan_to_num(np.exp(aj - ak))
+    s = np.nan_to_num(s)
+
+    return 1. / s
+
+def gradient(w, k, t, X):
+    output = np.zeros((len(w[0]), 1))
+    for n in range(len(X)):
+        scale = compute_y(n, k, w, X) - t[:, k][n]  # Ynk - Tnk
+
+        output += scale * phi(X[n])
+
+    return output
+
+def get_pred(dataX,w,n_class):
+    rePrediction = []
+    for data in dataX:
+        probability_max = 0
+        max_label = -1
+        tag = 1
+        for i in range(n_class):
+            if(tag):
+                probability = 0
+                s = 0
+                a_i = w[i].T.dot(data.reshape(-1,1))   
+                for j in range(n_class):
+                    a_j = w[j].T.dot(data.reshape(-1,1))   
+                    s += np.exp(a_j - a_i)
+                probability = 1/s
+                if(probability>0.5):
+                    max_label = i
+                    tag = 0
+                elif(probability > probability_max):
+                    probability_max = probability
+                    max_label = i
+        rePrediction.append(max_label)
+    return np.array(rePrediction)
 
 def get_err(data, w, y, allk):   
     s = 0
@@ -61,20 +95,22 @@ def get_err(data, w, y, allk):
     return s
 
 def get_hess(data, w ,k):
-    matr = np.zeros((w.shape[1],w.shape[1]))
-    for n in range(data.shape[0]):
-        s = get_y(data,n,k,w) *  (1 - get_y(data,n,k,w))
-        hereX = data[n]
-        matr += (float(s) * np.outer(hereX,hereX))
-    return matr
+    return hessian(w, k, data)
+    #matr = np.zeros((w.shape[1],w.shape[1]))
+    #for n in range(data.shape[0]):
+    #    s = get_y(data,n,k,w) *  (1 - get_y(data,n,k,w))
+    #    hereX = data[n]
+    #    matr += (float(s) * np.outer(hereX,hereX))
+    #return matr
 
 def get_grad(data, w, y, k):  
-    re = np.zeros((w.shape[1],1))
-    for n in range(data.shape[0]):
-        s = get_y(data,n,k,w) - y[:,k][n]
-        hereX = data[n].reshape(-1,1)
-        re += float(s) * hereX
-    return re
+    return  gradient(w, k, y, data)
+    #re = np.zeros((w.shape[1],1))
+    #for n in range(data.shape[0]):
+    #    s = get_y(data,n,k,w) - y[:,k][n]
+    #    hereX = data[n].reshape(-1,1)
+    #    re += float(s) * hereX
+    #return re
 
 def GD(trainData,trainLabel,trainLabel_oneHot,testData,testLabel,testLabel_oneHot,gdType,PCA_d = -1): 
     
@@ -173,7 +209,16 @@ def GD(trainData,trainLabel,trainLabel_oneHot,testData,testLabel,testLabel_oneHo
        
     return
 
-
+def predicts(w, x, classes):
+    softmaxes = []
+    for k in range(classes):
+        s = np.float64(0.)
+        ak = w[k].T.dot(phi(x))
+        for j in range(classes):
+            aj = w[j].T.dot(phi(x))
+            s += np.nan_to_num(np.exp(aj - ak))
+        softmaxes += [1./s]
+    return np.where(np.array(softmaxes).reshape(-1) > 1/ classes, 1, 0)
 def Raphson_Newton(trainData,trainLabel,trainLabel_oneHot,testData,testLabel,testLabel_oneHot,gdType,PCA_d = 10):
     
     if(PCA_d > 0):
@@ -183,8 +228,7 @@ def Raphson_Newton(trainData,trainLabel,trainLabel_oneHot,testData,testLabel,tes
         newTestData = testData - meanInTrain    
         out = newTestData.dot(pcaSpace)
         testData = out.astype('float64')
-    print(trainData.shape)
-    print(trainLabel.shape)
+
     trainAcc = []
     testAcc = []
     trainLoss = []
@@ -197,22 +241,47 @@ def Raphson_Newton(trainData,trainLabel,trainLabel_oneHot,testData,testLabel,tes
         err += get_err(trainData, w, trainLabel_oneHot, n_class)
         for k in range(n_class):
             w[k] -= inv(get_hess(trainData, w ,k)).dot(get_grad(trainData, w, trainLabel_oneHot, k))
-            print("gre:", get_grad(trainData, w, trainLabel_oneHot, k))
+        prediction = []
+
         if(epoch>=0):
             right = 0
             for row in range(len(trainLabel)):
-                if(trainLabel[row] == get_pred(trainData[row],w,n_class)):
+                if(trainLabel[row] == get_pred(trainData,w,n_class)[row]):
                     right += 1
-
-                    
-            print(right,"g")
-            trainAcc.append(right/trainData.shape[0])
         
-    plt.figure()    
-    plt.title(gdType + " _Accuracy")
-    plt.plot(trainAcc,label='train')
-    #plt.plot(testAcc,label='test')
-    plt.legend()
+                    
+            trainAcc.append(right/trainData.shape[0])
+    print(trainAcc)
+
+    x_min, x_max = trainData[:, 0].min() - 1, trainData[:, 0].max() + 1
+    y_min, y_max = trainData[:, 1].min() - 1, trainData[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.02),np.arange(y_min, y_max, 0.02))
+
+    print(np.array([xx.ravel(), yy.ravel()]).T.shape)
+    # here "model" is your model's prediction (classification) function
+
+  
+
+    Z = get_pred(np.array([xx.ravel(), yy.ravel()]).T,w,n_class)
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.contourf(xx, yy, Z,alpha= 0.3)
+
+    #for oneX in trainData.shape[0]:
+    #    if()
+    plt.scatter(trainData[:,0],trainData[:,1],marker = 'o' ,alpha=0.8,c=get_pred(trainData,w,n_class))
+    plt.scatter(testData[:,0],testData[:,1],marker = 'x',alpha=0.8,c=testLabel)
+    plt.axis('off')
+    
+    # Plot also the training points
+    #plt.scatter(X[:, 0], X[:, 1], c=Y, cmap=pl.cm.Paired)
+    #
+    #plt.figure()    
+    #plt.title(gdType + " _Accuracy")
+    #plt.plot(trainAcc,label='train')
+    ##plt.plot(testAcc,label='test')
+    #plt.legend()
     plt.show()
     
 def PCA(data, d):
@@ -228,10 +297,6 @@ def PCA(data, d):
     w = 28
     h = 28
     
-    for i in range(dEigen.T.shape[0]):
-        print("eigenvector ", i+1)
-        plt.imshow(np.array(dEigen.T[i].reshape(h,w),dtype = 'float32'),cmap = 'gray')        
-        plt.show()
     newVector = np.asarray(newVector)
     
     return newVector.astype('float32'),data_mean,dEigen
@@ -285,7 +350,7 @@ testLabel_oneHots = np.eye(n_values)[testLabels]
 #PCA(trainData,5)
 #GD(PCA(trainData,10),trainLabel,trainLabel_oneHot,PCA(testData,10),testLabel,testLabel_oneHot,"batch GD")
 
-Raphson_Newton(trainDatas,trainLabels,trainLabel_oneHots,testDatas,testLabels,testLabel_oneHots,"SGD",5)
+Raphson_Newton(trainDatas,trainLabels,trainLabel_oneHots,testDatas,testLabels,testLabel_oneHots,"SGD",2)
 #GD(trainDatas,trainLabels,trainLabel_oneHots,testDatas,testLabels,testLabel_oneHots,"SGD",5)
 #GD(trainData,trainLabel,trainLabel_oneHot,testData,testLabel,testLabel_oneHot,"SGD")
 
